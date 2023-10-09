cc857ee887136b23b441511547d6a0823d1d3d30
Pinalli <betopinalli@gmail.com>
2023-10-09 16:17:39 -0300

    update controller

---
+++ todolist/src/main/java/br/com/pinalli/todolist/user/UserController.java
@@ -0,0 +1,13 @@
+package br.com.pinalli.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    public void create() {
+        
+    }
+}
