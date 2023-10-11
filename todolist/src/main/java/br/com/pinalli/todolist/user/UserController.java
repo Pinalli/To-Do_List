a66dafea02f55c44ae504d0ce1ecc81e9a7e7e2f
pinalli <betopinali@gmail.com>
2023-10-11 18:59:50 -0300

    password crypt

--- todolist/src/main/java/br/com/pinalli/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/pinalli/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +25,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
         }
+        //password hashing
+        var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHash);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
